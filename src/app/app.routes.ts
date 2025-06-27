7efc3930788c4a5272fddf35bd2c168049ff5aeb
Lucas <lucasjaskowiak@yahoo.fr>
2025-06-27 09:19:32 +0200

    ajout du httpprovider à app.config et ajout du composant signin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,21 @@ import { RoomComponent } from './pages/room/room.component';
 import { LoginComponent } from './pages/login/login.component';
 import { CreateRoomComponent } from './pages/create-room/create-room.component';
 import { P404Component } from './pages/p404/p404.component';
+import { EditRoomComponent } from './pages/edit-room/edit-room.component';
+import { modoGuard } from './services/modo.guard';
+import { SigninComponent } from './pages/signin/signin.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'room', component: RoomComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'create-room', component: CreateRoomComponent },
+  { path: 'signin', component: SigninComponent },
+  {
+    path: 'create-room',
+    component: CreateRoomComponent,
+    canActivate: [modoGuard],
+  },
+  { path: 'edit-room', component: EditRoomComponent, canActivate: [modoGuard] },
 
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', component: P404Component },
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { NotifService } from '../../services/notif.service';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  formBuilder = inject(FormBuilder);
+  notification = inject(NotifService);
+  router = inject(Router);
+  http = inject(HttpClient);
+  authService = inject(AuthService);
 
+  formulaire = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.maxLength(50)]],
+  });
+
+  onConnection() {
+    this.http
+      .post('http://localhost:5000/login', this.formulaire.value, {
+        responseType: 'text',
+      })
+      .subscribe((jwt) => {
+        this.authService.decodeJwt(jwt);
+        this.notification.show('Vous êtes connecté', 'valid');
+        this.router.navigateByUrl('/home');
+      });
+  }
 }
