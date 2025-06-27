13bbed82c0c76875ba79934c48086bc0c43618ef
Lucas <lucasjaskowiak@yahoo.fr>
2025-06-27 09:33:44 +0200

    feat; ajout structure page inscription

--- src/app/pages/signin/signin.component.ts
+++ src/app/pages/signin/signin.component.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { NotifService } from '../../services/notif.service';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-signin',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './signin.component.html',
-  styleUrl: './signin.component.scss'
+  styleUrl: './signin.component.scss',
 })
 export class SigninComponent {
+  formBuilder = inject(FormBuilder);
+  notification = inject(NotifService);
+  router = inject(Router);
+  http = inject(HttpClient);
 
+  formulaire = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.maxLength(50)]],
+  });
+
+  onInscription() {
+    if (this.formulaire.valid) {
+      this.http
+        .post('http://localhost:5000/signin', this.formulaire.value)
+        .subscribe({
+          next: (res) => {
+            this.notification.show(
+              'Vous etes inscrit, vous pouvez vous connecter !',
+              'valid'
+            );
+            this.router.navigateByUrl('/connection');
+          },
+          error: (err) => {
+            if (err.status === 409) {
+              this.notification.show('Cet email est déjà utilisé', 'error');
+            }
+          },
+        });
+    }
+  }
 }
